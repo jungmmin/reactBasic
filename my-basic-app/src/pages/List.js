545cdc57115cb0d7848626765051f31d2e5cb8ca
정민 <ilb0919497@gmail.com>
2023-12-05 20:27:11 +0900

    List: 리스트 데이터 처리

---
+++ my-basic-app/src/pages/List.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function User({userData}){
+    return(
+        <tr>
+            <td>{userData.name}</td>
+            <td>{userData.email}</td>
+        </tr>
+    );
+}
+function List(){
+    const users = [
+        {email: 'ryu@gmail.com', name: '유재석'},
+        {email: 'kim@gmail.com', name: '김종국'},
+        {email: 'ha@gmail.com', name: '하하'},
+        {email: 'song@gmail.com', name: '송지효'}
+    ];
+
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>이름</th>
+                    <th>이메일</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user => <User userData={user} />)}
+            </tbody>
+        </table> 
+    );
+};
+export default List;
